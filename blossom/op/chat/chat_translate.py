bfa2cbf09430927322499f9910ab9486d24382dd
Azure99 <961523404@qq.com>
2024-09-08 20:38:18 +0800

    refactor: Update ChatTranslate operator to support multiple roles

--- blossom/op/chat/chat_translate.py
+++ blossom/op/chat/chat_translate.py
@@ -1,25 +1,17 @@
-from enum import Enum
 from typing import Any, Optional
 
 from blossom.op.map_operator import MapOperator
 from blossom.op.util.text_translator import TextTranslator
 from blossom.schema.base_schema import BaseSchema
-from blossom.schema.chat_schema import ChatMessage, ChatRole
+from blossom.schema.chat_schema import ChatRole
 
 
 class ChatTranslate(MapOperator):
-    class Role(Enum):
-        ALL = 0
-        SYSTEM = 1
-        USER = 2
-        ASSISTANT = 3
-        SYSTEM_USER = 4
-
     def __init__(
         self,
         translate_model: str,
         target_language: str = "Chinese",
-        role: Role = Role.ALL,
+        roles: Optional[list[ChatRole]] = None,
         instruction_only: bool = False,
         max_retry: int = 1,
         extra_params: Optional[dict[str, Any]] = None,
@@ -28,7 +20,9 @@ class ChatTranslate(MapOperator):
         super().__init__(parallel=parallel)
         self.translate_model = translate_model
         self.target_language = target_language
-        self.role = role
+        self.roles = (
+            roles if roles else [ChatRole.SYSTEM, ChatRole.USER, ChatRole.ASSISTANT]
+        )
         self.instruction_only = instruction_only
         self.max_retry = max_retry
         self.extra_params = extra_params
@@ -38,7 +32,7 @@ class ChatTranslate(MapOperator):
 
         translator = TextTranslator(self.context.get_model(self.translate_model))
         for message in _item.messages:
-            if self._check_translate_role(message):
+            if message.role in self.roles:
                 try:
                     message.content = translator.translate(
                         content=message.content,
@@ -51,17 +45,3 @@ class ChatTranslate(MapOperator):
                     message.content = ""
 
         return self._cast_base(_item)
-
-    def _check_translate_role(self, message: ChatMessage) -> bool:
-        if self.role == ChatTranslate.Role.ALL:
-            return True
-        elif self.role == ChatTranslate.Role.SYSTEM:
-            return message.role == ChatRole.SYSTEM
-        elif self.role == ChatTranslate.Role.USER:
-            return message.role == ChatRole.USER
-        elif self.role == ChatTranslate.Role.ASSISTANT:
-            return message.role == ChatRole.ASSISTANT
-        elif self.role == ChatTranslate.Role.SYSTEM_USER:
-            return message.role in [ChatRole.SYSTEM, ChatRole.USER]
-        else:
-            return False

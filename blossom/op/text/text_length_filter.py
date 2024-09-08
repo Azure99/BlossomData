49c32effda46124c82a73c5d954012878381fead
Azure99 <961523404@qq.com>
2024-09-08 19:08:05 +0800

    feat: Add TextLengthFilter operator for filtering text based on length

---
+++ blossom/op/text/text_length_filter.py
@@ -0,0 +1,19 @@
+from typing import Callable
+from blossom.op.filter_operator import FilterOperator
+from blossom.schema.base_schema import BaseSchema
+from blossom.schema.chat_schema import ChatRole
+
+
+class TextLengthFilter(FilterOperator):
+    def __init__(
+        self,
+        len_func: Callable[[str], int] = len,
+        max_len: int = 16384,
+    ):
+        super().__init__()
+        self.len_func = len_func
+        self.max_len = max_len
+
+    def process_item(self, item: BaseSchema) -> bool:
+        _item = self._cast_text(item)
+        return self.len_func(_item.content) <= self.max_len

148235b31387594be41142a34d83be86636fbc15
Azure99 <961523404@qq.com>
2024-09-08 19:10:57 +0800

    refactor: Update package

--- blossom/op/__init__.py
+++ blossom/op/__init__.py
@@ -1,12 +1,15 @@
 from .base_operator import BaseOperator
 from .chat.chat_distill import ChatDistill
 from .chat.chat_invalid_filter import ChatInvalidFilter
+from .chat.chat_length_filter import ChatLengthFilter
+from .chat.chat_math_distill import ChatMathDistill
 from .chat.chat_multi_reasoning_filter import ChatMultiReasoningFilter
 from .chat.chat_online_search_filter import ChatOnlineSearchFilter
 from .chat.chat_translate import ChatTranslate
 from .filter_operator import FilterOperator
 from .map_operator import MapOperator
 from .text.text_translate import TextTranslate
+from .text.text_length_filter import TextLengthFilter
 from .transform_operator import TransformOperator
 
 __all__ = [
@@ -15,9 +18,12 @@ __all__ = [
     "MapOperator",
     "TransformOperator",
     "ChatDistill",
+    "ChatLengthFilter",
+    "ChatMathDistill",
     "ChatMultiReasoningFilter",
     "ChatOnlineSearchFilter",
     "ChatTranslate",
     "ChatInvalidFilter",
     "TextTranslate",
+    "TextLengthFilter",
 ]
--- blossom/schema/__init__.py
+++ blossom/schema/__init__.py
@@ -1,10 +1,11 @@
-from .base_schema import BaseSchema
+from .base_schema import BaseSchema, SchemaType
 from .chat_schema import ChatSchema, ChatMessage, ChatRole
 from .custom_schema import CustomSchema
 from .text_schema import TextSchema
 
 __all__ = [
     "BaseSchema",
+    "SchemaType",
     "ChatSchema",
     "ChatMessage",
     "ChatRole",

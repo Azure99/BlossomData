30458bd06c96ab977ef9cb327f540ff4c6a56e86
Azure99 <i@rainng.com>
2024-12-20 23:08:18 +0800

    feat: Enhance ChatDistill to support response truncation and token limits

--- blossom/op/chat/chat_distill.py
+++ blossom/op/chat/chat_distill.py
@@ -3,8 +3,11 @@ from typing import Any, Optional
 from blossom.log import logger
 
 from blossom.op.map_operator import MapOperator
+from blossom.provider.protocol import ChatCompletionFinishReason
 from blossom.schema.base_schema import BaseSchema
 from blossom.schema.chat_schema import ChatMessage, ChatRole
+
+MATADATA_RESPONSE_TRUNCATED = "response_truncated"
 
 
 class ChatDistill(MapOperator):
@@ -17,6 +20,8 @@ class ChatDistill(MapOperator):
         self,
         model: str,
         strategy: Strategy = Strategy.FIRST_TURN,
+        max_total_tokens: Optional[int] = None,
+        stop_on_truncate: bool = False,
         max_retry: int = 1,
         extra_params: Optional[dict[str, Any]] = None,
         parallel: int = 1,
@@ -24,16 +29,20 @@ class ChatDistill(MapOperator):
         super().__init__(parallel=parallel)
         self.model = model
         self.strategy = strategy
+        self.max_total_tokens = max_total_tokens
+        self.stop_on_truncate = stop_on_truncate
         self.max_retry = max_retry
         self.extra_params = extra_params
 
     def process_item(self, item: BaseSchema) -> BaseSchema:
         _item = self._cast_chat(item)
 
-        new_messages = []
+        new_messages: list[ChatMessage] = []
         for _ in range(self.max_retry):
             try:
-                new_messages = self._process_item_messages(_item.messages)
+                new_messages, truncated = self._process_item_messages(_item.messages)
+                if truncated:
+                    _item.metadata[MATADATA_RESPONSE_TRUNCATED] = True
                 break
             except Exception as e:
                 logger.exception(f"Failed to distill chat: {e}")
@@ -45,7 +54,9 @@ class ChatDistill(MapOperator):
 
         return self._cast_base(_item)
 
-    def _process_item_messages(self, messages: list[ChatMessage]) -> list[ChatMessage]:
+    def _process_item_messages(
+        self, messages: list[ChatMessage]
+    ) -> tuple[list[ChatMessage], bool]:
         if self.strategy == ChatDistill.Strategy.FIRST_TURN:
             return self._process_first_turn(messages)
         elif self.strategy == ChatDistill.Strategy.LAST_TURN:
@@ -55,36 +66,64 @@ class ChatDistill(MapOperator):
 
         raise NotImplementedError("Distill strategy not implemented")
 
-    def _process_first_turn(self, messages: list[ChatMessage]) -> list[ChatMessage]:
+    def _process_first_turn(
+        self, messages: list[ChatMessage]
+    ) -> tuple[list[ChatMessage], bool]:
         messages = list(filter(lambda x: x.role != ChatRole.ASSISTANT, messages))
         for i, message in enumerate(messages):
             if message.role == ChatRole.USER:
                 messages = messages[: i + 1]
                 break
 
-        return self._add_assistant_reply(messages)
+        new_messages, truncated, _ = self._add_assistant_reply(messages)
+        return new_messages, truncated
 
-    def _process_last_turn(self, messages: list[ChatMessage]) -> list[ChatMessage]:
+    def _process_last_turn(
+        self, messages: list[ChatMessage]
+    ) -> tuple[list[ChatMessage], bool]:
         for i, message in enumerate(reversed(messages)):
             if message.role == ChatRole.ASSISTANT:
                 cut_off_index = len(messages) - 1 - i
                 messages = messages[:cut_off_index]
                 break
 
-        return self._add_assistant_reply(messages)
+        new_messages, truncated, _ = self._add_assistant_reply(messages)
+        return new_messages, truncated
 
-    def _process_multi_turn(self, messages: list[ChatMessage]) -> list[ChatMessage]:
+    def _process_multi_turn(
+        self, messages: list[ChatMessage]
+    ) -> tuple[list[ChatMessage], bool]:
+        has_truncated = False
         messages = list(filter(lambda x: x.role != ChatRole.ASSISTANT, messages))
 
         new_messages = []
         for message in messages:
             new_messages.append(message)
             if message.role == ChatRole.USER:
-                new_messages = self._add_assistant_reply(new_messages)
+                new_messages, truncated, total_tokens = self._add_assistant_reply(
+                    new_messages
+                )
 
-        return new_messages
+                if truncated:
+                    has_truncated = True
+                    if self.stop_on_truncate:
+                        break
 
-    def _add_assistant_reply(self, messages: list[ChatMessage]) -> list[ChatMessage]:
-        return self.context.chat_completion_with_messages(
+                if self.max_total_tokens and total_tokens >= self.max_total_tokens:
+                    break
+
+        return new_messages, has_truncated
+
+    def _add_assistant_reply(
+        self, messages: list[ChatMessage]
+    ) -> tuple[list[ChatMessage], bool, int]:
+        response = self.context.chat_completion_with_details(
             model=self.model, messages=messages, extra_params=self.extra_params
         )
+        choice = response.choices[0]
+
+        new_messages = messages + [choice.message]
+        response_truncated = choice.finish_reason == ChatCompletionFinishReason.LENGTH
+        total_tokens = response.usage.total_tokens
+
+        return new_messages, response_truncated, total_tokens

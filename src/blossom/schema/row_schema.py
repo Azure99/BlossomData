ee2b05141b59abcf8e560f286af294044e12adea
Azure99 <961523404@qq.com>
2025-05-03 09:12:44 +0800

    fix: Set default factory for data in RowSchema

--- src/blossom/schema/row_schema.py
+++ src/blossom/schema/row_schema.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+from pydantic import Field
+
 from blossom.schema.schema import Schema
 
 SCHEMA_TYPE_ROW = "row"
@@ -7,7 +9,7 @@ SCHEMA_TYPE_ROW = "row"
 
 class RowSchema(Schema):
     type: str = SCHEMA_TYPE_ROW
-    data: dict[str, Any]
+    data: dict[str, Any] = Field(default_factory=dict)
 
     def __getitem__(self, key: str) -> Any:
         return self.data[key]
